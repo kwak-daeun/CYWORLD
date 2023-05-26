88c09f215ec7e09c72aba8d0e36ab8e04e8756b4
sihyu <sihyu@DESKTOP-C75CPBU>
2023-05-26 15:14:10 +0900

    20230526 1514 - sihyun 3

--- CYWORLD/src/main/java/user/controller/UserController.java
+++ CYWORLD/src/main/java/user/controller/UserController.java
@@ -1,5 +1,20 @@
 package user.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
 public class UserController {
 
+	// 로그 객체
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	// 테스트
+	@RequestMapping("/user")
+	public void main() {
+		logger.info("USER 확인");
+	}
 }
